d27b4af66dd39856e5718dc7d6d65e96b0972ad7
youness serrakhi <120403490+YounessSerrakhi@users.noreply.github.com>
2024-04-23 09:08:03 +0100

    update ,the readme

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-!pip install tensorflow
-
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
@@ -10,6 +8,7 @@ def load_data_from_json(file_path):
         data = json.load(json_file)
     X = []  # Liste pour stocker les données d'entrée (amplitudes)
     y = []  # Liste pour stocker les étiquettes de sortie (fuite ou non)
+    
 
     for entry in data:
         X.append(entry['amplitudes'])
