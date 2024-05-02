f62c0599065bba2d95765ac5250c6cd85885913c
FATIMA ASEBBANE <126819418+fatimaasebbane@users.noreply.github.com>
2024-05-02 23:46:36 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+pip install tensorflow
+pip install numpy
+pip install sklearn.model_selection
 import json
 import numpy as np
 import tensorflow as tf
@@ -7,9 +10,8 @@ from sklearn.model_selection import train_test_split
 def load_data_from_json(file_path, max_length=None):
     with open(file_path, "r") as json_file:
         data = json.load(json_file)
-        
-    x = []  # Liste pour stocker les données d'entrée (amplitudes)
-    y = []  # Liste pour stocker les étiquettes de sortie (fuite ou non)
+    x_data = []  # Liste pour stocker les données d'entrée (amplitudes)
+    y_data = []  # Liste pour stocker les étiquettes de sortie (fuite ou non)
     for entry in data:
         amplitudes = entry['amplitudes']
         # Padding or trimming amplitudes to max_length
@@ -18,14 +20,14 @@ def load_data_from_json(file_path, max_length=None):
                 amplitudes += [0.0] * (max_length - len(amplitudes))
             elif len(amplitudes) > max_length:
                 amplitudes = amplitudes[:max_length]
-        x.append(amplitudes)
-        y.append(entry['leak'])
-    return np.array(x), np.array(y)
+        x_data.append(amplitudes)
+        y_data.append(entry['leak'])
+    return np.array(x_data), np.array(y_data)
 
 # Spécifier le chemin du fichier JSON contenant les données
 JSON_FILE_PATH = 'dataSet.json'
 
-# Charger les données à partir du fichier JSON avec padding/truncation 
+# Charger les données à partir du fichier JSON avec padding/truncation
 # à une longueur maximale de 200 (par exemple)
 x, y = load_data_from_json(JSON_FILE_PATH, max_length=200)
 
@@ -38,7 +40,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 
 # Définir le modèle DNN
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=(len(x[0]), 1)),
+    tf.keras.layers.Conv1D(filters=32, kernel_size=3, activation='relu',
+                           input_shape=(len(x[0]), 1)),
     tf.keras.layers.MaxPooling1D(pool_size=2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
